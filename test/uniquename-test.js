756c3a2e59d2701cc2da2e93c4e7ca7386260e40
Dumitru Cantea <canteadumitru@gmail.com>
2015-07-14 11:06:26 +0300

    config, eslint
    unique name throw error on invalid input

--- test/uniquename-test.js
+++ test/uniquename-test.js
@@ -1,50 +1,60 @@
+'use strict';
+
 var assert = require('chai').assert;
 var instance = require('../lib');
 
 describe('uniquename', function() {
-  it('null - should be undefined', function() {
-    assert.equal(instance.uniquename(null), undefined);
-  });
-  it('undefined - should be undefined', function() {
-    assert.equal(instance.uniquename(), undefined);
-  });
-  it('empty - should be undefined', function() {
-    assert.equal(instance.uniquename(''), undefined);
-    assert.equal(instance.uniquename('    '), undefined);
-  });
-  it('too small text', function() {
-    assert.throws(function() {
-      assert.equal(instance.uniquename('1  '), '1');
-    });
-    assert.throws(function() {
-      assert.equal(instance.uniquename('  a  '), 'a');
-    });
-  });
+	it('null - should be undefined', function() {
+		assert.throws(function() {
+			instance.uniquename(null);
+		});
+	});
+	it('undefined - should be undefined', function() {
+		assert.throws(function() {
+			instance.uniquename();
+		});
+	});
+	it('empty - should be undefined', function() {
+		assert.throws(function() {
+			instance.uniquename('');
+		});
+		assert.throws(function() {
+			instance.uniquename('    ');
+		});
+	});
+	it('too small text', function() {
+		assert.throws(function() {
+			assert.equal(instance.uniquename('1  '), '1');
+		});
+		assert.throws(function() {
+			assert.equal(instance.uniquename('  a  '), 'a');
+		});
+	});
 
-  it('`C++` = `c-plus-plus`', function() {
-    assert.equal('c-plus-plus', instance.uniquename('C++'));
-  });
+	it('`C++` = `c-plus-plus`', function() {
+		assert.equal('c-plus-plus', instance.uniquename('C++'));
+	});
 
-  it('`C#` = `c-sharp`', function() {
-    assert.equal('c-sharp', instance.uniquename('C#'));
-  });
+	it('`C#` = `c-sharp`', function() {
+		assert.equal('c-sharp', instance.uniquename('C#'));
+	});
 
-  it('`Node.js` = `nodejs`', function() {
-    assert.equal('nodejs', instance.uniquename('Node.js'));
-  });
+	it('`Node.js` = `nodejs`', function() {
+		assert.equal('nodejs', instance.uniquename('Node.js'));
+	});
 
-  it('`A++`=`a` throw error', function() {
-    assert.throws(function() {
-      instance.uniquename('A++    ');
-    });
-  });
+	it('`A++`=`a` throw error', function() {
+		assert.throws(function() {
+			instance.uniquename('A++    ');
+		});
+	});
 
-  it('`@A[/o~±ÆЙЖ_=?><`=`a-o-aeиж`', function() {
-    assert.equal(instance.uniquename('@A[/o~±ÆЙЖ_=?><'), 'a-o-aeиж');
-  });
+	it('`@A[/o~±ÆЙЖ_=?><`=`a-o-aeиж`', function() {
+		assert.equal(instance.uniquename('@A[/o~±ÆЙЖ_=?><'), 'a-o-aeиж');
+	});
 
-  it('`Начиная с версии Юникода`=`начиная-с-версии-юникода`', function() {
-    assert.equal(instance.uniquename('Начиная с версии Юникода'), 'начиная-с-версии-юникода');
-  });
+	it('`Начиная с версии Юникода`=`начиная-с-версии-юникода`', function() {
+		assert.equal(instance.uniquename('Начиная с версии Юникода'), 'начиная-с-версии-юникода');
+	});
 
 });
